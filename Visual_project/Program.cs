2eed233b2eb8ec8469a32c5b5b15d59c3185e126
venkey <venkeyvenkatesh885@gmail.com>
2020-09-24 13:01:55 +0530

    [venkey] Refactor. message change in Demo

--- Visual_project/Program.cs
+++ Visual_project/Program.cs
@@ -6,11 +6,7 @@ namespace Visual_project
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Hello World!DEMO");
-=======
             Console.WriteLine("Hello World!1234");
->>>>>>> b14de0a618d319ed1cfb45981c5f111abd417a2d
         }
     }
 }
