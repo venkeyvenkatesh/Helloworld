12e93aeba8e5b5c7838ed21aa1d627e48785f6db
venkeyvenkatesh <42239849+venkeyvenkatesh@users.noreply.github.com>
2020-09-24 13:34:09 +0530

    [venkey] Refactor. Change in master

--- Visual_project/Program.cs
+++ Visual_project/Program.cs
@@ -7,6 +7,7 @@ namespace Visual_project
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!DEMO");
+            Console.WriteLine("Second");
         }
     }
 }
